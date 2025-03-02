51623e45b1ec4d55af87b81991ab93ff540a588a
ANANDAKRISHNAN-MS <i.am.anandakrishnan.m.s@gmail.com>
2025-03-02 12:11:27 +0530

    Feed Screen

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,7 +1,16 @@
-import { Text } from "react-native"
+import { FlatList } from "react-native"
+
+import posts from "~/assets/data/posts.json"
+import PostListItem from "~/src/components/PostListItem"
+
 
 export default function FeedScreen() {
-    return(
-        <Text> Feed </Text>
-    )
+   return(
+    <FlatList
+        data={posts}
+        renderItem={({ item }) => <PostListItem post={ item }/>}
+        contentContainerStyle={{gap: 10}}
+        showsVerticalScrollIndicator={false}
+    />
+   )
 }
