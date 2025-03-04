cc2b971246f1f5ff56cdcb88890c239e45c745ba
ANANDAKRISHNAN-MS <i.am.anandakrishnan.m.s@gmail.com>
2025-03-04 19:56:18 +0530

    add AuthProvider

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,10 +1,22 @@
+import { useEffect, useState } from "react"
 import { FlatList } from "react-native"
 
-import posts from "~/assets/data/posts.json"
+// import posts from "~/assets/data/posts.json"
 import PostListItem from "~/src/components/PostListItem"
 
 
 export default function FeedScreen() {
+   const [posts, setPosts] = useState([]);
+
+   useEffect(() => {
+        fetchPosts();
+   }, [])
+
+   const fetchPosts = async () => {
+        let data = /* data to be fetched */
+        setPosts([])
+   }
+
    return(
     <FlatList
         data={posts}
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,9 @@
 import { Stack } from "expo-router";
 import "../../global.css"
+import AuthProvider from "../providers/AuthProvider";
 
 export default function RootLayout(){
-    return < Stack screenOptions={{headerShown: false}}/>
+    <AuthProvider>
+       return < Stack screenOptions={{headerShown: false}}/> 
+    </AuthProvider>
 }
